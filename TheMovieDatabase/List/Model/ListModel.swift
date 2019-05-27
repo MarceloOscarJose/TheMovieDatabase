08416abdfcf45aa352d6247864b052b394a86c18
Marcelo Oscar José <marcelo.oscar.jose@gmail.com>
2019-05-27 04:12:19 -0300

    Pages in lists

--- TheMovieDatabase/List/Model/ListModel.swift
+++ TheMovieDatabase/List/Model/ListModel.swift
@@ -12,36 +12,52 @@ class ListModel: NSObject {
 
     let listService = ListService()
     var currentScope: ConfigData.Scope!
-
-    var currentPages: [Int]!
+    var currentPages: [ListModelPages] = []
 
     var params: [String: String] = ["page" : "1"]
 
     convenience init(scope: ConfigData.Scope) {
         self.init()
         self.currentScope = scope
-        self.currentPages = Array(repeating: 1, count: scope.data.count)
+        self.currentPages = scope.data.map { _ in ListModelPages(currentPage: 1, maxPages: 1) }
     }
 
     func getList<T: Codable>(nextPage: Bool, query: String, scope: Int, entity: T.Type, responseHandler: @escaping (_ response: [ListModelData]) -> Void, errorHandler: @escaping (_ error: Error?) -> Void) {
-        currentPages[scope] = nextPage ? currentPages[scope] + 1 : 1
+        currentPages[scope].currentPage = nextPage ? currentPages[scope].currentPage + 1 : 1
         params["page"] = "\(currentPages[scope])"
         params["query"] = query
 
         listService.fetchList(url: currentScope.data[scope].url, entity: entity, params: params, responseHandler: { (result) in
 
+            var responseList: [ListModelData] = []
+            var totalPages: Int = 1
+
             switch self.currentScope.id {
             case "movies":
-                responseHandler((result as! MovieListResponse).results.map( { ListModelData(movie: $0) }))
+                let responseData = (result as! MovieListResponse)
+                totalPages = responseData.totalPages
+                responseList = responseData.results.map( { ListModelData(movie: $0) })
             case "shows":
-                responseHandler((result as! ShowListResponse).results.map( { ListModelData(show: $0) }))
+                let responseData = (result as! ShowListResponse)
+                totalPages = responseData.totalPages
+                responseList = responseData.results.map( { ListModelData(show: $0) })
             case "search":
-                responseHandler((result as! SearchListResponse).results.map( { ListModelData(result: $0) }))
+                let responseData = (result as! SearchListResponse)
+                totalPages = responseData.totalPages
+                responseList = responseData.results.map( { ListModelData(result: $0) })
             default:
                 break
             }
+
+            self.currentPages[scope].maxPages = totalPages
+            responseHandler(responseList)
         }) { (error) in
             errorHandler(error)
         }
     }
+}
+
+struct ListModelPages {
+    var currentPage: Int
+    var maxPages: Int
 }
